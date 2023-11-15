4d65a2304cc5db7d817406da0730c310098399a4
Denilson Arguelho <denilsons.arguelho@gmail.com>
2023-11-15 19:23:17 -0400

    requisição axios

--- src/routes/HomeClient/ProductDetails/index.tsx
+++ src/routes/HomeClient/ProductDetails/index.tsx
@@ -7,6 +7,9 @@ import ProductDetailsCard from "../../../components/ProductDetailsCard";
 import * as productServices from "../../../services/product-services";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { ProductDTO } from "../../../models/product";
+import axios from "axios";
 
 /* const product: ProductDTO = {
   id: 2,
@@ -34,14 +37,31 @@ import { Link } from "react-router-dom";
 const ProductDetails = () => {
   const params = useParams();
 
-  const product = productServices.findById(Number(params.productId));
+  const [product, setProduct] = useState<ProductDTO>();
+
+  useEffect(() => {
+    /* Requisição na apí, trazendo detalhes de produtos */
+   axios.get("http://localhost:8080/products/1").then(response =>{
+    console.log(response);
+    
+   }).catch(error => {
+    console.log("Erro Axios",error);
+    
+   })
+
+    const prod = productServices.findById(Number(params.productId));
+    setProduct(prod);
+  }, []);
+
   return (
     <main>
       <section id="product-details-section" className="dsc-container">
         {product && <ProductDetailsCard product={product} />}
         <div className="dsc-btn-page-container">
           <ButtonPrimary text="Comprar" />
-          <Link to={`/`}><ButtonSecondary text="Inicio" /></Link>
+          <Link to={`/`}>
+            <ButtonSecondary text="Inicio" />
+          </Link>
         </div>
       </section>
     </main>
