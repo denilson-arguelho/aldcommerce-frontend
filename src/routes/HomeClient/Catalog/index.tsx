9e5d53c7825acd786a17978b5125c0fb5797785f
Denilson Arguelho <denilsons.arguelho@gmail.com>
2023-11-16 19:27:27 -0400

    requisições axios

--- src/routes/HomeClient/Catalog/index.tsx
+++ src/routes/HomeClient/Catalog/index.tsx
@@ -4,16 +4,29 @@ import "./style.css";
 import SearchBar from "../../../components/SearchBar";
 import CatalogCard from "../../../components/CatalogCard";
 import CatalogNextPage from "../../../components/ButtonNextPage";
-/* Importando todas as funçoes do product-services */
-import * as productService from "../../../services/product-services";
+import { useEffect, useState } from "react";
+import { ProductDTO } from "../../../models/product";
 
-const Catalog = () => {
+import * as productService from '../../../services/product-services'
+
+export default function Catalog() {
+
+
+const [product, setProduct] = useState<ProductDTO[]>([]);
+
+useEffect(()=>{
+  productService.findAll()
+  .then(response => {
+    setProduct(response.data.content)
+  })
+},[])
+
   return (
     <main>
       <section id="catalog-section" className="dsc-container">
         <SearchBar />
         <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
-          {productService.findAll().map((product) => (
+          {product.map((product) => (
             <CatalogCard key={product.id} product={product} />
           ))}
         </div>
@@ -21,6 +34,4 @@ const Catalog = () => {
       </section>
     </main>
   );
-};
-
-export default Catalog;
+}
--- src/routes/HomeClient/ProductDetails/index.tsx
+++ src/routes/HomeClient/ProductDetails/index.tsx
@@ -3,55 +3,26 @@ import "./style.css";
 import ButtonPrimary from "../../../components/ButtonPrimary";
 import ButtonSecondary from "../../../components/ButtonSecondary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
+import * as productService from '../../../services/product-services'
 
-import * as productServices from "../../../services/product-services";
-import { useParams } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { ProductDTO } from "../../../models/product";
-import axios from "axios";
 
-/* const product: ProductDTO = {
-  id: 2,
-  name: "Smart TV",
-  description: "Essa TV é muito Bonita",
-  imgUrl:
-    "https://github.com/devsuperior/dscatalog-resources/blob/master/backend/img/2-big.jpg?raw=true",
-  price: 2500.0,
-  categories: [
-    {
-      id: 2,
-      name: "Eletrônicos",
-    },
-    {
-      id: 3,
-      name: "Computadores",
-    },
-    {
-      id: 4,
-      name: "Importados",
-    },
-  ],
-}; */
+
 
 const ProductDetails = () => {
-  const params = useParams();
+   const params = useParams(); 
 
   const [product, setProduct] = useState<ProductDTO>();
 
   useEffect(() => {
     /* Requisição na apí, trazendo detalhes de produtos */
-   axios.get("http://localhost:8080/products/1").then(response =>{
-    console.log(response);
-    
-   }).catch(error => {
-    console.log("Erro Axios",error);
-    
+   productService.findById(Number(params.productId)).then(response =>{
+   setProduct(response.data)
    })
 
-    const prod = productServices.findById(Number(params.productId));
-    setProduct(prod);
-  }, []);
+  }, [params]);
 
   return (
     <main>
